c944e36ca0793a8713f9bbc06be6673f0927ec98
Lporchart <pegasusryuseiken2.0@gmail.com>
2021-02-22 23:56:23 -0300

    Continuação Aula 3

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// transactons = transaction e viser se versa no codigo do professro
+// tempo 1:45:30
+
+
 const Modal = {
     open(){
         document.querySelector('.modal-overlay').classList.add('active')
@@ -14,29 +18,37 @@ const transaction = [
 ]
 
 const Transaction = {
-    iconmes(){
+    all: transaction,
+    add(transaction){
+        Transaction.all.push(transaction)
+        App.reload()
+    },
 
+
+    inconmes(){
+        let income = 0;
+        Transaction.all.forEach(transaction =>{
+            if( transaction.amount > 0){
+                income += transaction.amount;
+            }
+        })
+        return income
     },
     expenses(){
-
+        let expense = 0;
+        Transaction.all.forEach(transaction =>{
+            if( transaction.amount < 0){
+                expense += transaction.amount;
+            }
+        })
+        return expense
     },
     total(){
+        let expense = Transaction.expenses(); 
+        let income = Transaction.inconmes();
+        let total = income + expense
 
-    }
-}
-
-const Utils = {
-    formatCurrency(value){
-        const signal = Number(value) < 0 ? "-" : ""
-        value = String(value).replace(/\D/g, "")
-        console.log(value)
-        value = Number(value) / 100
-        value = value.toLocaleString("pt-BR",{
-            style: "currency",
-            currency:"BRL"
-            }            
-        )
-       return signal + value 
+        return total
     }
 }
 
@@ -60,8 +72,51 @@ const DOM = {
             <td><img src="./assets/minus.svg" alt="remover Transação"></td>
             `
         return html
+    },
+    updateBalance(){
+        document.getElementById('incomedisplay').innerHTML = Utils.formatCurrency(Transaction.inconmes())
+        document.getElementById('expensedisplay').innerHTML = Utils.formatCurrency(Transaction.expenses())
+        document.getElementById('totaldisplay').innerHTML = Utils.formatCurrency(Transaction.total())
+    },
+    clearTransactions(){
+        DOM.transactionContainer.innerHTML = ""
     }
 }
-transaction.forEach(function(transaction){
-    DOM.addTransaction(transaction)
+
+const Utils = {
+    formatCurrency(value){
+        const signal = Number(value) < 0 ? "-" : ""
+        value = String(value).replace(/\D/g, "")       
+        value = Number(value) / 100
+        value = value.toLocaleString("pt-BR",{
+            style: "currency",
+            currency:"BRL"
+            }            
+        )
+       return signal + value 
+    }
+}
+
+
+const App = {
+    init(){
+        Transaction.all.forEach(transaction => {
+            DOM.addTransaction(transaction)
+        })
+
+        DOM.updateBalance()
+    },
+    reload(){
+        DOM.clearTransactions()
+        App.init()
+    }
+}
+
+App.init()
+
+Transaction.add({
+    id : 32,
+    description: 'feijao no prato',
+    amount : 200,
+    date : '12/12/2016'
 })
